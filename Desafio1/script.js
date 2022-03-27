898f6eafc1aa7c605780f9e5ea5bb7eee4b62ea3
AlexandreCamposs <89165912+AlexandreCamposs@users.noreply.github.com>
2022-03-27 09:47:23 -0300

    att

---
+++ Desafio1/script.js
@@ -0,0 +1,10 @@
+document.querySelector('.tecla1');
+document.querySelector('.tecla2');
+document.querySelector('.tecla3');
+document.querySelector('.tecla4');
+document.querySelector('.tecla5');
+document.querySelector('.tecla6');
+document.querySelector('.tecla7');
+document.querySelector('.tecla8');
+
+document.querySelector('input[type=tel]');
